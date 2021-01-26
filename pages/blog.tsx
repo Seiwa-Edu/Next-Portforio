fa31b509c572f15dad1103a466cba22d472471c2
Seiwa-Edu <kuma.0131@icloud.com>
2021-01-26 23:24:22 +0900

    add the microcms API

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,16 +1,57 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { GetStaticProps } from 'next'
+import { TableBody } from '@material-ui/core'
+import React from 'react';
 
-const BlogPage = () => (
+type Props = {
+  id: number;
+  title: string;
+  body: string;
+}
+
+//ここに原因があるかな？blogsの渡し方
+const BlogPage = ({blogs}) => (
     <Layout title="Blog | Seiwa Blog">
-      <h1>About</h1>
-      <p>This is the about page</p>
-      <p>
-        <Link href="/">
-          <a>Go home</a>
-        </Link>
-      </p>
+      <div>
+        <div>
+          {/* カード要素 */}
+          { blogs.map(blog => (
+              <div className="card">
+                  <div className="card__imgframe"></div>
+                  <div className="card__textbox">
+                    <div className="card__titletext">
+                      <p>{blog.title}</p>
+                    </div>
+                    <div className="card__overviewtext">
+                      <p>{blog.body}</p>
+                    </div>
+                  </div>
+              </div>
+          ))}
+          {/* カード要素 */}
+        </div>
+      </div>
     </Layout>
   )
   
+    // APIキーの取得
+    //  export async function getStaticProps(context) {
+    // export const GetStaticProps = async () => {
+    export const getStaticProps: GetStaticProps = async context => {
+    const key = {
+      // headers: {'X-API-KEY': process.env.API_KEY},
+      headers: {'X-API-KEY': '92887c9a-4cc2-45c9-addc-1074f7676411'},
+    };
+    const data = await fetch('https://seiwa-portfolio.microcms.io/api/v1/blog',key)
+    .then( res => res.json())
+    .catch(()=> null);
+    return {
+      props: {
+        blog: data.contents,
+      },
+      
+    };
+  };
+
   export default BlogPage
