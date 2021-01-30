b49bc0bf807277d01a90810f6a5e0fc3a1a7d322
Seiwa-Edu <kuma.0131@icloud.com>
2021-01-30 09:49:16 +0900

    [add] introduction messages

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,39 +13,43 @@ export default function Home() {
                 <div className={style_css.intro__image}><img src="" className={style_css.intro__code}/></div>
                 <div className={style_css.intro__textBox}>
                   <h2>Seiwa</h2>
-                  <p>趣味は温泉と二郎と開発。</p>
+                  <p>1998/01/31　生まれ</p>
+                  <p>高校まで岩手県盛岡市で生活し、大学では微生物を活用した遺伝子の研究を行う。</p>
+                  <p>2020年3月 友人とのAR開発がきっかけでプログラミングを始める。</p>
+                  <p>趣味: 温泉と二郎と開発</p>
                 </div>
               </div>
             </div>
           </section>
           <section className={style_css.skill_section}>
-            <div>
-              <h2>Skill</h2>
-              <h3>Frontend</h3>
+            <div className={style_css.skill__wrapper}>
+              <h2 className={style_css.section_title}>Skill</h2>
+              <div className={style_css.programming_wrapper}>
+                 <h3 className={style_css.programming_cate}>Frontend</h3>
+              </div>              
               <p>HTML/CSS</p>
               <p>JavaScript React</p>
-              <h3>Backend</h3>
+              <h3 className={style_css.programming_cate}>Backend</h3>
               <p>Ruby on Rails</p>
-              <h3>infrastructure</h3>
+              <div>
+                 <h3 className={style_css.programming_cate}>infrastructure</h3>
+              </div>   
               <p>AWS</p>
             </div>
           </section>
           <section className={style_css.works_section}>
-         　 <div>
-              <h2>Works</h2>
-
+         　 <div className={style_css.skill__wrapper}>
+              <h2 className={style_css.section_title}>Works</h2>
               <a>もっと見る</a>
             </div>
           </section>
           <section className={style_css.blog_section}>
-          <div>
-              <h2>Blogs</h2>
-              
+          <div className={style_css.skill__wrapper}>
+              <h2 className={style_css.section_title}>Blogs</h2>
               <a>もっと見る</a>
             </div>
           </section>
         </div>
       </Layout>
-
   )
 }
