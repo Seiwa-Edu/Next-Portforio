0d4002b87b821ed4eee3dfdf5dfbebabcd1c9de5
Seiwa-Edu <kuma.0131@icloud.com>
2021-02-17 23:45:57 +0900

    [update]update the text

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,82 +9,82 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-      <Layout title="Home | Seiwa Blog">
-        <div>
-          <section className={style_css.intro_section}>
-            <div className={style_css.intro__wrapper}>
-              <div className={style_css.intro__content}>
-                <div className={style_css.intro__image}><img src="undraw_Powerful_re_frhr.svg" className={style_css.intro__code}/></div>
-                <div className={style_css.intro__textBox}>
-                  <h2>Seiwa</h2>
-                  {/* <p>1998/01/31　生まれ</p> */}
-                  <p>岩手県盛岡市育ち。大学では微生物を活用した遺伝子の研究を行う。</p>
-                  <p>同郷の友人達とのAR開発を行ったことがきっかけでプログラミングを始める。</p>
-                  <p>趣味: 温泉と二郎と開発</p>
-                </div>
+    <Layout title="Home | Seiwa Blog">
+      <div>
+        <section className={style_css.intro_section}>
+          <div className={style_css.intro__wrapper}>
+            <div className={style_css.intro__content}>
+              <div className={style_css.intro__image}><img src="undraw_Powerful_re_frhr.svg" className={style_css.intro__code} /></div>
+              <div className={style_css.intro__textBox}>
+                <h2>Seiwa</h2>
+                {/* <p>1998/01/31　生まれ</p> */}
+                <p>岩手県盛岡市育ち。大学では微生物を活用した遺伝子の研究を行う。</p>
+                <p>同郷の友人達とのAR開発を行ったことがきっかけでプログラミングを始める。</p>
+                <p>趣味: 温泉と二郎と開発</p>
               </div>
             </div>
-          </section>
-          <section className={style_css.skill_section}>
-            <div className={style_css.skill__wrapper}>
-              <h2 className={style_css.section_title}>Skill</h2>
+          </div>
+        </section>
+        <section className={style_css.skill_section}>
+          <div className={style_css.skill__wrapper}>
+            <h2 className={style_css.section_title}>Skill</h2>
 
-              <div className={style_css.programming_wrapper}>
-                 <h3 className={style_css.programming_cate}>Front-end</h3>
-              </div> 
+            <div className={style_css.programming_wrapper}>
+              <h3 className={style_css.programming_cate}>Front-end</h3>
+            </div>
 
-              <div className={style_css.programming__comp}>
-                {/* <div className={style_css.programming__image}><img src="HTML5_Badge.svg"/></div> */}
-                <div className={style_css.hello}>
+            <div className={style_css.programming__comp}>
+              {/* <div className={style_css.programming__image}><img src="HTML5_Badge.svg"/></div> */}
+              <div className={style_css.hello}>
                 <h4>HTML/CSS</h4>
                 <p>開発経験 約1年間。<br></br>
                    Webサイトの作成、WEBサービスの作成、デザインからのサイト作成も経験。</p>
-                </div>
-              </div>             
-              
-              <h4>JavaScript React</h4>
-              <p> React, Next js, TypeScript を中心に学習中</p>
-              <div className={style_css.programming_wrapper}>
-                 <h3 className={style_css.programming_cate}>Back-end</h3>
-              </div> 
-              <div className={style_css.programming__comp}>
-                {/* <div className={style_css.programming__image}><img src="rails-1.svg" width="150px"/></div> */}
-                <div className={style_css.hello}>
+              </div>
+            </div>
+
+            <h4>JavaScript React</h4>
+            <p> React, Next js, TypeScript を中心に学習中</p>
+            <div className={style_css.programming_wrapper}>
+              <h3 className={style_css.programming_cate}>Back-end</h3>
+            </div>
+            <div className={style_css.programming__comp}>
+              {/* <div className={style_css.programming__image}><img src="rails-1.svg" width="150px"/></div> */}
+              <div className={style_css.hello}>
                 <h4>Ruby on Rails</h4>
-                <p>開発経験 約3、４ヶ月<br></br>
+                <p>開発経験 約４ヶ月<br></br>
                 WEBサービス作成中</p>
-                </div>
-              </div>     
-              <div>               
-                 <h3 className={style_css.programming_cate}>infrastructure</h3>
-              </div>   
-              <h4>AWS</h4>
-              <p>勉強中</p>
-            </div>
-          </section>
-          <section className={style_css.works_section}>
-         　 <div className={style_css.skill__wrapper}>
-              <h2 className={style_css.section_title}>Work</h2>
-              <div className={card_css.cardlayout_wrap}>
-                  <Card title ="自己紹介サイト" id ="work/nkgjap1lrf" body="Nextjs TypeScriptでサイト作成" image="team-5614157_1280.png"/>
               </div>
-              <Link href="/works">     
-                 <a className={style_css.detail__link}>もっと見る</a>
-              </Link>           
             </div>
-          </section>
-          <section className={style_css.blog_section}>
+            <div>
+              <h3 className={style_css.programming_cate}>infrastructure</h3>
+            </div>
+            <h4>AWS</h4>
+            <p>クラウドプラクティショナー </p>
+          </div>
+        </section>
+        <section className={style_css.works_section}>
           <div className={style_css.skill__wrapper}>
-              <h2 className={style_css.section_title}>Blog</h2>
-              <div className={card_css.cardlayout_wrap}>
-                <Card title ="Hello world" id ="blog/yaurrrnlm" body="microcms動作確認" image="team-5614157_1280.png"/>
-              </div>
-              <Link href="/blog">
-              <a className={style_css.detail__link}>もっと見る</a>
-              </Link>
+            <h2 className={style_css.section_title}>Work</h2>
+            <div className={card_css.cardlayout_wrap}>
+              <Card title="自己紹介サイト" id="work/nkgjap1lrf" body="Nextjs TypeScriptでサイト作成" image="team-5614157_1280.png" />
             </div>
-          </section>
-        </div>
-      </Layout>
+            <Link href="/works">
+              <a className={style_css.detail__link}>もっと見る</a>
+            </Link>
+          </div>
+        </section>
+        <section className={style_css.blog_section}>
+          <div className={style_css.skill__wrapper}>
+            <h2 className={style_css.section_title}>Blog</h2>
+            <div className={card_css.cardlayout_wrap}>
+              <Card title="Hello world" id="blog/yaurrrnlm" body="microcms動作確認" image="team-5614157_1280.png" />
+            </div>
+            <Link href="/blog">
+              <a className={style_css.detail__link}>もっと見る</a>
+            </Link>
+          </div>
+        </section>
+      </div>
+    </Layout>
   )
 }
